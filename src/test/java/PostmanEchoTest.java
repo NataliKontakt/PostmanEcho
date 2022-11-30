456c07ca160ef962e203e7f32114c8d877e16d60
“Natali <natali-kontakt@yandex.ru>
2022-11-30 21:23:34 +0500

    fail

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("balance: 1,2,3"))
+                .body("data", equalTo("balance: 1,3,3"))
         ;
     }
 }
